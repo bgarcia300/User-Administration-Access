885754c51fd646fbd9183ef7918d6483e6fbb5c1
bayrongarcia300 <bayrongarcia300@gmail.com>
2020-04-20 21:53:34 -0600

    Agrego mas columnas a las tablas

--- Querys/Creacion_Tablas.sql
+++ Querys/Creacion_Tablas.sql
@@ -14,22 +14,32 @@ create table Usuarios
 (
 Id_Usuario int IDENTITY(1,1) PRIMARY KEY
 , Id_Rol int NOT NULL
-, constraint FK_ROLES_USUARIOS foreign key (Id_Rol)
-references Roles (Id_Rol)
-, Nombre_Completo_Usuario nvarchar(30) unique not null
-, Telefono int
+, Nombre_Usuario nvarchar(15) unique not null
+, Primer_Apellido_Usuario nvarchar(15) unique not null
+, Segundo_Apellido_Usuario nvarchar(15) unique not null
 , Funcion nvarchar(40)
 , Fecha_Nacimiento date not null
+, Edad AS (datediff(Day,Fecha_Nacimiento,getdate()))/(365)
+, Telefono int not null
+, Correo_Electronico nvarchar(40) not null
+, Residencia nvarchar(500) not null
+, observaciones nvarchar(500) null
+, constraint FK_ROLES_USUARIOS foreign key (Id_Rol)
+references Roles (Id_Rol)
 )
 go
-
 
 create table Administradores
 (
 Id_Administrador int IDENTITY(1,1) PRIMARY KEY
 , Id_Rol int NOT NULL
 , Id_Usuario int NOT NULL
-, Nombre_Completo_Administrador nvarchar(30) NOT NULL
+, Nombre_Administrador nvarchar(15) unique not null
+, Primer_Apellido_Administrador nvarchar(15) unique not null
+, Segundo_Apellido_Administrador nvarchar(15) unique not null
+, Telefono int not null
+, Correo_Electronico nvarchar(40) not null
+, observaciones nvarchar(500) null
 , constraint FK_ROLES_ADMINISTRADORES foreign key (Id_Rol)
 references Roles (Id_Rol)
 , constraint FK_USUARIOS_ADMINISTRADORES foreign key (Id_Usuario)
@@ -42,7 +52,12 @@ create table Directores
 Id_Director int IDENTITY(1,1) PRIMARY KEY
 , Id_Rol int NOT NULL
 , Id_Usuario int NOT NULL
-, Nombre_Completo_Director nvarchar(30) NOT NULL
+, Nombre_Director nvarchar(15) unique not null
+, Primer_Apellido_Director nvarchar(15) unique not null
+, Segundo_Apellido_Director nvarchar(15) unique not null
+, Telefono int not null
+, Correo_Electronico nvarchar(40) not null
+, observaciones nvarchar(500) null
 , constraint FK_ROLES_DIRECTORES foreign key (Id_Rol)
 references Roles (Id_Rol)
 , constraint FK_USUARIOS_DIRECTORES foreign key (Id_Usuario)
@@ -70,10 +85,19 @@ Id_Proyectos int IDENTITY(1,1) PRIMARY KEY
 , Id_Grupo int NOT NULL
 , Nombre_Proyecto nvarchar(40) NOT NULL
 , Porcentaje_Avance int NOT NULL
-, Estado_Proyecto nvarchar(15) NOT NULL
+, Estado_Proyecto AS ( 
+CASE 
+	WHEN Porcentaje_Avance >= 0 and Porcentaje_Avance <= 30 THEN 'Comenzando' 
+	WHEN Porcentaje_Avance >= 31 and Porcentaje_Avance <= 75 THEN 'En Proceso' 
+	WHEN Porcentaje_Avance >= 76 and Porcentaje_Avance <= 99 THEN 'Finalizando' 
+	WHEN Porcentaje_Avance = 100 THEN 'Completado' 
+	ELSE 'No se tiene conocimiento del estado'
+END
+)
 , constraint FK_GRUPOS_PROYECTOS foreign key (Id_Grupo)
 references GRUPOS (Id_Grupo)
 )
+go
 
 create table BitacoraAvanceProyectos
 (
@@ -84,7 +108,7 @@ Id_Bitacor_Avance_Proyecto int IDENTITY(1,1) PRIMARY KEY
 , Hora_Registro_Avance time NOT NULL
 , Porcentaje_Avance numeric(9,2) NOT NULL
 , Estado_Avance varchar(50) NOT NULL
-, Observacion varchar(200) NOT NULL
+, Observacion varchar(500) NOT NULL
 )
 go
 
@@ -96,5 +120,5 @@ Id_Bitacora_Usuario int IDENTITY(1,1) PRIMARY KEY
 , Fecha_Registro_Modificacion date NOT NULL
 , Hora_Registro_Modificacion time(0) NOT NULL
 , Tabla_Rol_Alterada varchar(50) NOT NULL
-, Observacion varchar(200)
+, Observacion varchar(500)
 )
